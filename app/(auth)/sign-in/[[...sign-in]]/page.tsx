e94cd558cff8645f8f1ab9045e621b8f3b7c14fe
Priyanshu090803 <priyanshu122a@gmail.com>
2025-07-26 21:54:57 +0530

    hide the clerk logo and tag

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,16 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className=' h-screen w-full flex items-center justify-center'>
+      <SignIn
+     appearance={{
+          elements: {
+            footer: 'hidden', // hides the "Don't have an account?" link
+            cardFooter: 'hidden', // hides the "Secured by Clerk" + "Development mode"
+          },
+        }}
+      />
+    </div>
+  )
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,16 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return (
+  <div className=' h-screen w-full flex items-center justify-center'>
+        <SignUp
+        appearance={{
+          elements: {
+            footer: 'hidden', // hides the "Don't have an account?" link
+            cardFooter: 'hidden', // hides the "Secured by Clerk" + "Development mode"
+          },
+        }}
+        />
+      </div>
+      )
 }
